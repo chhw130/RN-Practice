7446a6d45dd85a79c5d7ffc33ea74948c4075252
최현우 <hw.choi@okestro.com>
2023-12-27 18:42:54 +0900

    first commit

---
+++ first-practice/App.tsx
@@ -0,0 +1,19 @@
+import { Button, StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="tap" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
