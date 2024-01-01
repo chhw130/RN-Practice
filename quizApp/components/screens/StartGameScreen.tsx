7c33e8f0f632c1387e3e26402088f92318b38642
HyunWoo Choi <116826162+chhw130@users.noreply.github.com>
2024-01-01 18:20:27 +0900

    feat : styling shadow ios & android

--- quizApp/components/screens/StartGameScreen.tsx
+++ quizApp/components/screens/StartGameScreen.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Text, TextInput, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import MainButton from "../MainButton";
 
 const StartGameScreen = () => {
   return (
-    <View>
+    <View style={styles.inputContainer}>
       <TextInput />
       <MainButton>Reset</MainButton>
       <MainButton>Start!</MainButton>
@@ -13,3 +13,23 @@ const StartGameScreen = () => {
 };
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#72063c",
+    marginTop: 30,
+    padding: 15,
+    borderRadius: 10,
+    marginHorizontal: 20,
+    /**ios */
+    shadowColor: "black",
+    shadowOffset: { width: 20, height: 20 },
+    shadowRadius: 20,
+    shadowOpacity: 0.5,
+
+    /**android */
+    elevation: 30,
+  },
+});
