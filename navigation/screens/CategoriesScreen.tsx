b91fcde77c13c45443bfd198ac1eabd593672445
mincho <527coco@naver.com>
2024-01-26 19:42:08 +0900

    feat : display item list

--- navigation/screens/CategoriesScreen.tsx
+++ navigation/screens/CategoriesScreen.tsx
@@ -3,8 +3,10 @@ import { FlatList } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
 
-const renderCategoryItem = () => {
-  return <CategoryGridTile />;
+const renderCategoryItem = (itemData) => {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
 };
 
 const CategoriesScreen = () => {
@@ -12,7 +14,7 @@ const CategoriesScreen = () => {
     <FlatList
       data={CATEGORIES}
       keyExtractor={(item) => item.id}
-      renderItem={}
+      renderItem={renderCategoryItem}
     />
   );
 };
