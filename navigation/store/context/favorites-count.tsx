7988e40698c405d899d436b0c4d4255528f0b143
mincho <527coco@naver.com>
2024-02-05 20:44:17 +0900

    feat : setting state context api

--- navigation/store/context/favorites-count.tsx
+++ navigation/store/context/favorites-count.tsx
@@ -1,4 +1,4 @@
-import { ReactDOM, ReactNode, createContext } from "react";
+import { ReactDOM, ReactNode, createContext, useState } from "react";
 
 interface FavoritesContextType {
   ids: string[];
@@ -6,15 +6,33 @@ interface FavoritesContextType {
   removeFavorite: (id: string) => void;
 }
 
-export const FavoritesContext = createContext<FavoritesContextType | null>({
+export const FavoritesContext = createContext<FavoritesContextType>({
   ids: [],
   addFavorite: (id) => {},
   removeFavorite: (id) => {},
 });
 
 const FavoritesContextProvider = ({ children }: { children: ReactNode }) => {
+  const [favoriteMealIds, setFavoriteMealIds] = useState<string[]>([]);
+
+  const addFavorite = (id: string) => {
+    setFavoriteMealIds((currentFavIds) => [...currentFavIds, id]);
+  };
+
+  const removeFavorite = (id: string) => {
+    setFavoriteMealIds((currentFavIds) =>
+      currentFavIds.filter((mealId) => mealId !== id)
+    );
+  };
+
+  const value = {
+    ids: favoriteMealIds,
+    addFavorite,
+    removeFavorite,
+  };
+
   return (
-    <FavoritesContext.Provider value={null}>
+    <FavoritesContext.Provider value={value}>
       {children}
     </FavoritesContext.Provider>
   );
